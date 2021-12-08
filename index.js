f452fecbc182a4ef87c4f886fb5a60ad66b72707
Sean Craig <seancraig21@gmail.com>
2021-12-08 09:55:02 -0600

    The csv is now parsed into an array

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const uploadForm = document.getElementById("upload-form");
 const csvFile = document.getElementById("csv-file");
 
+function csvToArray(str, delimiter = ",") {
+  // slice from start of text to the first \n index
+  // use split to create an array from string to delimiter
+  const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
+
+  // slice from /n index + 1 to the end of the text
+  // use split to create an array of each csv value row
+  const rows = str.slice(str.indexOf("\n") + 1).split("\n");
+
+  // Map the rows
+  // split values from each row into array
+  // use headers.reduce to create object
+  // object properties derived from headers (values)
+  // the object passed as an element of the array
+  const resultsArray = rows.map(function (row) {
+    const values = row.split(delimiter);
+    const element = headers.reduce(function (object, header, index) {
+      object[header] = values[index];
+      return object;
+    }, {});
+    return element;
+  });
+  return resultsArray;
+}
+
 uploadForm.addEventListener("submit", function (event) {
   // prevents the default behavior of the page refreshing
   event.preventDefault();
@@ -14,7 +39,9 @@ uploadForm.addEventListener("submit", function (event) {
 
   // Once the reading is done
   reader.onload = function (event) {
-    document.write(event.target.result); // The CSV content as string
+    const text = event.target.result;
+    const data = csvToArray(text);
+    document.write(JSON.stringify(data)); // The CSV content as string
   };
 
   reader.readAsText(input);
