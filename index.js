9592cda1812e145fb07d9fb406b4490cd4201624
Sean Craig <seancraig21@gmail.com>
2021-12-09 15:59:49 -0600

    table is almost fully working

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const uploadForm = document.getElementById("upload-form");
 const csvFile = document.getElementById("csv-file");
-const countsObj = {};
+const tableEl = document.getElementById("table");
+const noHitsObj = {};
+const hitsObj = {};
 
 function csvToArray(str, delimiter = ",") {
   // slice from start of text to the first \n index
@@ -29,16 +31,56 @@ function csvToArray(str, delimiter = ",") {
 
 function getWordCount(array) {
   for (const query of array) {
-    if (countsObj.hasOwnProperty(query.query)) {
-      countsObj[query.query].counts++;
+    if (query.hits === "0") {
+      if (noHitsObj.hasOwnProperty(query.query)) {
+        noHitsObj[query.query].counts++;
+      } else {
+        noHitsObj[query.query] = {
+          counts: 1,
+          hits: query.hits,
+        };
+      }
     } else {
-      countsObj[query.query] = {
-        counts: 1,
-        hits: query.hits,
-      };
+      if (hitsObj.hasOwnProperty(query.query)) {
+        hitsObj[query.query].counts++;
+      } else {
+        hitsObj[query.query] = {
+          counts: 1,
+          hits: query.hits,
+        };
+      }
     }
   }
-  return countsObj;
+}
+
+function displayTable(data) {
+  console.log(data);
+  let tHeadEl = tableEl.createTHead();
+  // let row = tableEl.insertRow();
+  let th1 = document.createElement("th");
+  let th1Text = document.createTextNode("Query");
+  let th2 = document.createElement("th");
+  let th2Text = document.createTextNode("Counts");
+
+  th1.appendChild(th1Text);
+  th2.appendChild(th2Text);
+  tHeadEl.appendChild(th1);
+  tHeadEl.appendChild(th2);
+
+  // console.log(Object.keys(data))
+
+  for (const value in data) {
+    let tr = document.createElement("tr");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let query = document.createTextNode(value);
+    let counts = document.createTextNode("1");
+    td1.appendChild(query);
+    td2.appendChild(counts);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tHeadEl.appendChild(tr);
+  }
 }
 
 uploadForm.addEventListener("submit", function (event) {
@@ -58,8 +100,9 @@ uploadForm.addEventListener("submit", function (event) {
     const data = csvToArray(text);
     //document.write(JSON.stringify(data));
     // console.log(Object.keys(data));
-    const resultsObj = getWordCount(data);
-    console.log(resultsObj);
+    getWordCount(data);
+    displayTable(noHitsObj);
+    console.log(hitsObj);
   };
 
   reader.readAsText(input);
