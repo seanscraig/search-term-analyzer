46c89633e9cc74c865c669dd197819724bd16711
Sean Craig <seancraig21@gmail.com>
2021-12-16 19:53:52 -0600

    added a few more comments

--- index.js
+++ index.js
@@ -2,34 +2,43 @@ const uploadForm = document.getElementById("upload-form");
 const csvFile = document.getElementById("csv-file");
 const tableEl = document.getElementById("zero-table");
 const chartEl = document.getElementById("query-chart");
+
 const noHitsObj = {};
 const hitsObj = {};
 
 function csvToArray(str) {
-  //
+  // the character that will separate each element
   const delimiter = ",";
 
   // this first array will contain the property keys from the first line of the file
   // slice from start of text to the first \n index
   // use split to create an array from string to delimiter
   const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
-  console.log(headers);
 
   // this second array will contain the values
   // slice from /n index + 1 to the end of the text
   // use split to create an array of each csv value row
   const rows = str.slice(str.indexOf("\n") + 1).split("\n");
 
-  // Map the rows
+  // Map over the rows
   const resultsArray = rows.map((line) => {
-
-    //
+    // a new array that contains the line separated into groups by the delimiter
     const eachGroup = line.split(delimiter);
-    // console.log(eachGroup);
-    //
+
+    // edge case for if the line includes a "
     if (line.includes('"')) {
+      // create a new variable to hold the contents of the search term between the quotes
       const extractQuote = line
+        // using the string match method, it targets a string and accepts a regular expression, it returns anything that is selected from the target string as "matching" the expression
+        // the expression itself uses the ? character to represent the start of the string and the $ to represent the end of the string
+        // the [] characters are used to enclose a series of characters, and the ^ means not. The | within the [] means either pattern is valid
+        // so either a character exists after the quotation mark, or before it
+        // the * on both sides of the | means "any number of matching characters"
+        // the match method returns all found matches as an array, so we target index 0 to indicate the first match
         .match(/(?:"[^"]*"|^[^"]*$)/)[0]
+        // at this point we have "\"Some text in quote\"", so we replace all the " characters using the replace string method
+        // the replace method accepts a regex targeting a portion of the string, and a second argument of what to replace it with
+        // this regex says: find all " characters and replace them with nothing
         .replace(/"/g, "");
 
       return {
@@ -40,17 +49,19 @@ function csvToArray(str) {
         [headers[4]]: eachGroup[5],
         [headers[5]]: eachGroup[6],
       };
+      // line does not contain a " so create object with key value pairs
     } else {
+      // use the array method reduce with a supplied callback function to create the element as an object
       const element = headers.reduce(function (object, header, index) {
         // object properties derived from headers (values)
         object[header] = eachGroup[index];
-        console.log(index);
         return object;
       }, {});
+      // return object as element of array
       return element;
     }
   });
-  console.log(resultsArray);
+
   return resultsArray;
 }
 
@@ -85,8 +96,10 @@ function getWordCount(array) {
 }
 
 function sortData(dataObj) {
+  // create an array of the values from the object so we can iterate over them
   const valuesArrToItr = Object.values(dataObj);
 
+  // call the sort array method with a specified compare function
   valuesArrToItr.sort(function (a, b) {
     if (a["counts"] < b["counts"]) {
       return 1;
@@ -166,7 +179,7 @@ uploadForm.addEventListener("submit", function (event) {
   // create a new instance of FileReader class so the file can be read
   const reader = new FileReader();
 
-  // reads the
+  // reads the csv file as a string
   reader.readAsText(input);
 
   // Once the reading operation is successful
@@ -180,14 +193,14 @@ uploadForm.addEventListener("submit", function (event) {
     // count the queries and put them into two different objects depending if they have hits or not
     getWordCount(data);
 
-    //
+    // sort the data in both of the hits objects
     const noHitsSortedData = sortData(noHitsObj);
     const hitsSortedData = sortData(hitsObj);
 
-    //
+    // displays the table using the no hits sorted data
     displayTable(noHitsSortedData);
 
-    //
+    // displays the chart using the hits sorted data
     displayChart(hitsSortedData);
   };
 });
